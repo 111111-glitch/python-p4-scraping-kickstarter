6c7f1904a22b2ca799f8ae1b29c012d84313b70e
111111-glitch <ngigimelissa@gmail.com>
2024-04-06 16:00:51 +0300

    complete

--- lib/kickstarter_scraper.py
+++ lib/kickstarter_scraper.py
@@ -1,31 +1,34 @@
 from bs4 import BeautifulSoup
 import ipdb
 
-# projects: kickstarter.select("li.project.grid_4")[0]
-# title: project.select("h2.bbcard_name strong a")[0].text
-# image link: project.select("div.project-thumbnail a img")[0]['src']
-# description: project.select("p.bbcard_blurb")[0].text
-# location: project.select("ul.project-meta span.location-name")[0].text
-# percent_funded: project.select("ul.project-stats li.first.funded strong")[0].text.replace("%","")
-
 def create_project_dict():
     html = ''
     with open('./fixtures/kickstarter.html') as file:
         html = file.read()
+
     kickstarter = BeautifulSoup(html, 'html.parser')
     projects = {}
+    
     # Iterate through the projects
     for project in kickstarter.select("li.project.grid_4"):
         title = project.select("h2.bbcard_name strong a")[0].text
-        projects[title] = {
-        'image_link': project.select("div.project-thumbnail a img")[0]["src"],
-        'description': project.select("p.bbcard_blurb")[0].text,
-        'location': project.select("ul.project-meta span.location-name")[0].text,
-        'percent_funded': project.select("ul.project-stats li.first.funded strong")[0].text.replace("%","")
-        }
-    # return the projects dictionary
+        # Extract information for each project
+        image_link = project.select("div.project-thumbnail a img")[0]['src']
+        description = project.select("p.bbcard_blurb")[0].text
+        location = project.select("ul.project-meta span.location-name")[0].text
+        percent_funded = project.select("ul.project-stats li.first.funded strong")[0].text.replace("%", "")
 
+        # Add project information to the projects dictionary
+        projects[title] = {
+            'image_link': image_link,
+            'description': description,
+            'location': location,
+            'percent_funded': percent_funded
+        }
+
+    # Return the projects dictionary
     return projects
 
+# Call the function and print the projects
 projects = create_project_dict()
 print(projects)
